c1fe7c6a42cc01ab9a557e5602d293827ba136c8
EmilUlrikChristoffersen <emilulrikchristoffersen@outlook.dk>
2016-11-30 10:47:22 +0100

    fixes

--- src/main/java/REST/FlightFacade.java
+++ src/main/java/REST/FlightFacade.java
@@ -1,52 +1,45 @@
 package REST;
 
-
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 import Entity.Flight;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
  * @author LouiseB
  */
-public class FlightFacade
-{
+public class FlightFacade {
+
     EntityManagerFactory emf;
 
-    public FlightFacade()
-    {
+    public FlightFacade() {
+        this.emf = Persistence.createEntityManagerFactory("Airline");
     }
 
-    public FlightFacade(EntityManagerFactory emf)
-    {
+    public FlightFacade(EntityManagerFactory emf) {
         this.emf = emf;
     }
 
-    public List<Flight> getAllFlights()
-    {
+    public List<Flight> getAllFlights() {
 
         EntityManager em = emf.createEntityManager();
 
         List<Flight> flight = null;
 
-        try
-        {
+        try {
             em.getTransaction().begin();
             flight = em.createQuery("SELECT f FROM Flight f").getResultList();
             em.getTransaction().commit();
             System.out.println(flight);
             return flight;
-        } finally
-        {
+        } finally {
             em.close();
         }
     }
