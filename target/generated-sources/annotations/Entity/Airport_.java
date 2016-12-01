2c465907148db16f9fd99fceef69a7957713c6df
EmilUlrikChristoffersen <emilulrikchristoffersen@outlook.dk>
2016-12-01 11:30:08 +0100

    fixed_dependecy

--- target/generated-sources/annotations/Entity/Airport_.java
+++ target/generated-sources/annotations/Entity/Airport_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-01T11:05:34")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-01T11:26:00")
 @StaticMetamodel(Airport.class)
 public class Airport_ { 
 
