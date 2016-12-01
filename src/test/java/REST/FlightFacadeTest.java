8861a03eb750fd109b558a75256f977b53f82ee9
Lange-Laptop-Pc\Lange-Laptop <peterleolange@yahoo.dk>
2016-12-01 10:40:14 +0100

    lavet test men ikke færdigt

--- src/test/java/REST/FlightFacadeTest.java
+++ src/test/java/REST/FlightFacadeTest.java
@@ -8,6 +8,9 @@ package REST;
 import Entity.Airline;
 import Entity.Airport;
 import Entity.Flight;
+import Entity.FlightInstance;
+import Entity.FlightInstance_;
+import java.sql.Date;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -65,15 +68,11 @@ public class FlightFacadeTest {
     {
         FlightFacade instance = new FlightFacade();
         Flight expResult = null; 
-        List<Flight> result = instance.getAllFlights();
-        result.get(0).setFlightInstanceCollection(null);
-        result.get(0).getAirline().setFlightCollection(null);
-        result.get(0).getfromAirport().setFlightCollection(null);
-        result.get(0).getfromAirport().setFlightCollection1(null);
-        result.get(0).gettooAirport().setFlightCollection(null);
-        result.get(0).gettooAirport().setFlightCollection1(null);
-        expResult = new Flight("newy001", 200, 5, null, new Airline(1, "TestLine1", null), new Airport("TestCode1", "CET", "Copenhagen Airport", "Denmark", "Copenhagen", null, null), new Airport("TestCode2", "EST", "john f kennedy international airport", "USA", "New York", null, null));
-        assertEquals(expResult.toString(), result.get(0).toString());
+        List<FlightInstance> result = instance.getFlightsWithFromDateTickets("CPH", new Date(2012,1,28), 0);
+        result.get(0).setavailableSeats(0);
+        
+        expResult = new FlightInstance(new Date(2012,1,28), 1, 23, null, null);
+                assertEquals(expResult.toString(), result.get(0).toString());
     }
 
 }
